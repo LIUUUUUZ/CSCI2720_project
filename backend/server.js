b382f741dd39e619108db404850c2df929ca117d
HiFei4869 <107867164+HiFei4869@users.noreply.github.com>
2023-12-13 17:44:33 +0800

    Update server.js
    Upload (user) log-in & sign-up

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const fs = require('fs')
 
+// (testing) import the User model
+const User = require('./userSchema.js');
+
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -123,6 +126,70 @@ db.once('open', () => {
       });
     });
 
+ // Sign-up
+    app.post('/api/signup', async (req, res) => {
+        try {
+            console.log('Received Request Body:', req.body);
+            const { username, password } = req.body;
+
+            // Check if the username is already taken
+            const existingUser = await User.findOne({ username });
+            if (existingUser) {
+                return res.status(401).json({ message: 'The user already exists. Please log in.' });
+            }
+
+            // Create a new user using testing data without hashing the password
+            const newUser = new User({
+                username,
+                password, // Store the password as plain text
+                isAdmin: false,
+                favoriteVenueID: []
+            });
+
+            // Save the user to the database
+            await newUser.save();
+
+            // Return the user information in the response
+            res.json({
+                username: newUser.username,
+                isAdmin: newUser.isAdmin,
+                favoriteVenueID: newUser.favoriteVenueID
+            });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    });
+// Log-in
+    app.post('/api/login', async (req, res) => {
+        try {
+            const { username, password } = req.body;
+
+            // Check if the user exists
+            const user = await User.findOne({ username });
+
+            if (!user) {
+                return res.status(401).json({ message: 'No user credential found. Please sign up.' });
+            }
+
+            // Check if the password is correct
+            const isPasswordValid = (password==user.password);
+
+            if (!isPasswordValid) {
+                return res.status(401).json({ message: 'Authentication failed. Please try again.' });
+            }
+
+            // Return the user information in the response
+            res.json({
+                username: user.username,
+                isAdmin: user.isAdmin,
+                favoriteVenueID: user.favoriteVenueID
+            });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    });
 
     // 存入测试数据 testVenueInfo.json
     app.get('/test-location-list', (req, res) => {
@@ -152,12 +219,36 @@ db.once('open', () => {
         res.status(500).json({ error: 'Internal Server Error' });
       });
     });
+    // 存入测试数据 testUserInfo.json
+    app.get('/test-user-list', (req, res) => {
+        const filePath = './public/testUserInfo.json';
 
+        function readFileAsync(filePath) {
+            return new Promise((resolve, reject) => {
+                fs.readFile(filePath, 'utf8', (err, data) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve(JSON.parse(data));
+                });
+            });
+        }
 
-
-
-
-
+        readFileAsync(filePath)
+            .then((data) => {
+                for (let i = 0; i < data.length; i++) {
+                    // Create a new user instance and save it to the database
+                    const user = new User(data[i]);
+                    user.save();
+                }
+                res.json(data);
+            })
+            .catch((err) => {
+                console.error('Error reading user file:', err);
+                res.status(500).json({ error: 'Internal Server Error' });
+            });
+    });
 
 
 
