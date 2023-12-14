927390e899a4a3fd79fcb8c0dd3ea7be75415dbe
Yitiao <1912468701@qq.com>
2023-12-15 02:42:43 +0800

    Finished location page CSS

--- frontend/src/components/LocationPage.js
+++ frontend/src/components/LocationPage.js
@@ -6,7 +6,8 @@ import axios from 'axios';
 const SERVER_URL = 'localhost:5555'
 const colorSet = {
   CUHKPurple: "#740f6B",
-  CUHKYellow: "#E6B001"
+  CUHKYellow: "#E6B001",
+  iconGrey: "#888888"
 }
 
 function LocationPage({user, setUser}) {
@@ -21,6 +22,7 @@ function LocationPage({user, setUser}) {
 
   function noMatchedLocationID() {
     /* To be finished*/
+    console.log(1)
     navigate("/")
   };
 
@@ -39,22 +41,16 @@ function LocationPage({user, setUser}) {
   function showComments() {
     setShownContainer('comments')
     const scrollYPercentage = window.scrollY / window.innerHeight * 100
-    if (scrollYPercentage > 60) {
-      const targetScrollY = 0.6 * window.innerHeight;
-      window.scrollTo({
-        top: targetScrollY
-      });
+    if (scrollYPercentage > 0) {
+      window.scrollTo(0, 0);
     }
   }
 
   function showEvents() {
     setShownContainer('events')
     const scrollYPercentage = window.scrollY / window.innerHeight * 100
-    if (scrollYPercentage > 60) {
-      const targetScrollY = 0.6 * window.innerHeight;
-      window.scrollTo({
-        top: targetScrollY
-      });
+    if (scrollYPercentage > 0) {
+      window.scrollTo(0, 0);
     }
   }
 
@@ -89,12 +85,45 @@ function LocationPage({user, setUser}) {
         </div>
         <MapContainer position={location_map}/>
         <div id='location-info-container' className='location-info-container'>
-          <div id='location-button-line-container' className='location-button-line-container-start'>
+          <div id='location-button-line-container' className='location-button-line-container location-button-line-container-start'>
+            
             {isFavorite ?
-            <button onClick={addToFavorite}>Remove from favorite</button> :
-            <button onClick={addToFavorite}>Add to favorite</button>}
-            <button onClick={showEvents}>Events</button>
-            <button onClick={showComments}>Comments</button>
+            <div className='location-button' onClick={addToFavorite}>
+              <div className='location-button-border' style={{borderColor: colorSet.CUHKPurple}}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill={colorSet.CUHKPurple} className="bi bi-calendar-week" viewBox="0 0 16 16" style={{position: "relative", top: "2px"}}>
+                  <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+                </svg>
+              </div>
+              <p className='description'>Remove</p>
+            </div> :
+            <div className='location-button' onClick={addToFavorite}>
+              <div className='location-button-border'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill={colorSet.iconGrey} className="bi bi-calendar-week" viewBox="0 0 16 16" style={{position: "relative", top: "2px"}}>
+                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+              </svg>
+            </div>
+            <p className='description'>Favorite</p>
+          </div>}
+            <div className='border-div' style={{height: '7vh'}}></div>
+            <div className='location-button' onClick={showEvents}>
+              <div className='location-button-border' style={shownContainer === 'comments' ? {} : { borderColor: colorSet.CUHKPurple }}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill={shownContainer === 'comments' ? colorSet.iconGrey : colorSet.CUHKPurple} className="bi bi-calendar-week" viewBox="0 0 16 16">
+                  <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/>
+                  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
+                </svg>
+              </div>
+              <p className='description'>Events</p>
+            </div>
+            <div className='border-div' style={{height: '7vh'}}></div>
+            <div className='location-button' onClick={showComments}>
+              <div className='location-button-border' style={shownContainer === 'events' ? {} : { borderColor: colorSet.CUHKPurple }}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill={shownContainer === 'events' ? colorSet.iconGrey : colorSet.CUHKPurple} className="bi bi-calendar-week" viewBox="0 0 16 16" style={{position: "relative", top: "1px",}}>
+                  <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                  <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+                </svg>
+              </div>
+              <p className='description'>Comments</p>
+            </div>
           </div>
           <div id='location-info-body-container' className='location-info-body-container-start'>
             {shownContainer === 'events' ? <EventContainer location={location} /> : undefined}
@@ -163,7 +192,6 @@ function MapContainer(props) {
         hider.style.visibility = 'visible'
 
         cunningMargin.style.visibility = 'visible'
-      
       }
     };
 
@@ -189,7 +217,7 @@ function MapContainer(props) {
 };
 
 function EventContainer({location}) {
-  const [highestPrice, setHighestPrice] = useState(0)
+  const [highestPrice, setHighestPrice] = useState(9999999)
   const [shownEvents, setShownEvents] = useState([])
   const events = location.events
 
@@ -198,7 +226,8 @@ function EventContainer({location}) {
   }
 
   const resetHighestPrice = () => {
-    setHighestPrice(0)
+    document.getElementById("price-search").value = ""
+    setHighestPrice(9999999)
     setShownEvents(events)
   }
 
@@ -208,19 +237,37 @@ function EventContainer({location}) {
 
   return (
     <div>
-      <div id='location-second-line-container' className='location-second-line-container-start'>
-        <input id='price-filter' type='number' min={0} step={1} value={highestPrice} onChange={e => setHighestPrice(e.target.value)}></input>
-        <button onClick={filterHighestPrice}>Filter</button>
-        <button onClick={resetHighestPrice}>Reset</button>
-      </div>
-      {shownEvents.map(event => (
-        <div key={event.ID} style={{border: '2px, black, solid'}}>
-          <div>{event.time}</div>
-          <div>{event.description}</div>
-          <div>{event.presenter}</div>
-          <div>{event.price}</div>
+      <div id='location-second-line-container' className='location-second-line-container location-second-line-container-start'>
+        <input id='price-search' className="form-control me-2" style={{width: "40vw", }} type="search" placeholder="Set a price, then filter the events with lower price..." aria-label="Search" onChange={e => setHighestPrice(e.target.value)} />
+        <div className='second-button' onClick={filterHighestPrice}>
+          <div className='second-button-border'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill={colorSet.iconGrey} className="bi bi-calendar-week" viewBox="0 0 16 16" style={{position: "relative", top: "2px"}}>
+              <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2z"/>
+            </svg>
+          </div>
         </div>
-      ))}
+        <div className='second-button' onClick={resetHighestPrice}>
+          <div className='second-button-border'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill={colorSet.iconGrey} className="bi bi-calendar-week" viewBox="0 0 16 16" style={{position: "relative", top: "2px"}}>
+              <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
+              <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className='event-list-container'>
+        {shownEvents.map(event => (
+          <div key={event.ID} className='event-item-cell'>
+            <div className='event-item-presenter'>{event.presenter}</div>
+            <div className='event-item-price'><span style={{fontWeight: 'bold'}}>{event.price}</span> HKD</div>
+            <div className="event-item-timeslots-container">
+              <div className="event-item-timeslots-number">{event.time.length}</div>
+              <div className="event-item-timeslots-text">Timeslots</div>
+            </div>
+            <div className='event-item-description'>{event.description.length > 110 ? event.description.substring(0, 108) + '...' : event.description}</div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
@@ -228,31 +275,39 @@ function EventContainer({location}) {
 function CommentContainer({user, location, comments, setComments}) {
   const [newComment, setNewComment] = useState('')
 
-  async function submitNewComment() {
-    try {
-      const response = await axios.post(`http://${SERVER_URL}/api/add-comment`, {
-        locationID: location.ID,
-        username: user.username,  
-        text: newComment
-      })
-      setComments(response.data)
-    } catch (err) {
-      console.log(err)
+  async function submitNewComment(e) {
+    if (newComment !== '') {
+      e.preventDefault();
+      setNewComment('')
+      try {
+        const response = await axios.post(`http://${SERVER_URL}/api/add-comment`, {
+          locationID: location.ID,
+          username: user.username,  
+          text: newComment
+        })
+        setComments(response.data)
+        window.scrollTo(0, 99999999)
+      } catch (err) {
+        console.log(err)
+      }
     }
   }
 
   return (
     <div>
-      <div id='location-second-line-container' className='location-second-line-container-start'>
-        <textarea id='new-comment' value={newComment} onChange={e => setNewComment(e.target.value)} /><br/>
-        <button id='send' type='submit' onClick={e => submitNewComment(e)}>Send</button>
+      <div id='location-second-line-container' className='location-second-line-container location-second-line-container-start'>
+        <input id='new-comment' className="form-control me-2" style={{width: "50vw", }} type="text" placeholder="Your comment to this location..." value={newComment} onChange={e => setNewComment(e.target.value)} />
+        <button className="btn btn-outline-success" type="submit" onClick={e => submitNewComment(e)}>Send</button>
       </div>
+      <div className='event-list-container'>
       {comments.map((comment, index) => (
-        <div key={index} style={{border: '2px, black, solid'}}>
-          <div style={{fontWeight: 'bold'}}>{comment.username}</div>
-          <div>{comment.text}</div>
-        </div>
+          <div key={index} className='comment-item-cell'>
+            <div style={{fontWeight: 'bold', position: 'relative', top: '1vh', left: '2vh'}}>{comment.username}</div>
+            <div className='border-div' style={{width: '55vw', position: 'relative', top: '1vh', marginBottom: '0.5vh'}}></div>
+            <div style={{position: 'relative', top: '1vh', left: '2vh', marginBottom: '1.5vh'}}>{comment.text}</div>
+          </div>
       ))}
+      </div>
     </div>
   )
 }
